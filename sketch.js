708386a27c31a46959f7c9ebaf9c36edfdba3030
Tim Mozee <tim.mozee@warnerbros.com>
2019-06-01 00:28:16 -0700

    Marching along but turns into itself to  frequently

--- sketch.js
+++ sketch.js
@@ -39,7 +39,13 @@ function draw() {
     current.visited = true;
     let next = current.checkNeighbors();
     if (next) {
+        //step1
         next.visited = true;
+
+        //step 3
+        removeWalls(current, next);
+
+        //step 4
         current = next;
 
     }
@@ -123,12 +129,44 @@ function Cell(i, j) {
 
 
         if (this.visited) {
+            noStroke();
 
             fill(255, 0, 255, 100);
             rect(x, y, w, w);
 
 
         }
+    }
+
+}
+
+
+
+function removeWalls(a, b) {
+    let x = a.i - b.i;
+
+
+    if (x === 1) {
+        a.walls[3] = false;
+        b.walls[1] = false;
+
+    } else if (x === -1) {
+        a.walls[1] = false;
+        b.walls[3] = false;
+
+    }
+
+
+    let y = a.j - b.j
+
+    if (y === 1) {
+        a.walls[0] = false;
+        b.walls[2] = false;
+
+    } else if (y === -1) {
+        a.walls[2] = false;
+        b.walls[0] = false;
+
     }
 
 }
