3af6e9a02e25ca320643797897d17d2556cac45e
Tim Mozee <tim.mozee@warnerbros.com>
2019-06-01 00:07:55 -0700

    Marching along but blocking when no available neighbor

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 let cols, rows;
-let w = 100;
+let w = 30;
 let grid = [];
+
+let current;
 
 
 function setup() {
@@ -8,6 +10,8 @@ function setup() {
 
     cols = floor(width / w);
     rows = floor(height / w);
+
+    frameRate(5);
 
 
     for (let j = 0; j < rows; j++) {
@@ -19,6 +23,8 @@ function setup() {
         console.log(grid);
 
     }
+
+    current = grid[0];
 }
 
 
@@ -27,20 +33,74 @@ function draw() {
 
     for (let index = 0; index < grid.length; index++) {
         grid[index].show();
-        console.log(index);
+        // console.log(index);
+
+    }
+    current.visited = true;
+    let next = current.checkNeighbors();
+    if (next) {
+        next.visited = true;
+        current = next;
 
     }
 }
 
+function index(i, j) {
+    if (i < 0 || j < 0 || i > cols - 1 || j > rows - 1) {
+
+        return -1;
+
+    }
+
+    return i + (j * cols);
+}
 
 function Cell(i, j) {
 
     this.i = i;
     this.j = j;
     this.walls = [true, true, true, true];
+    this.visited = false;
+
+    //Mantra
+    //top, right, bottom, left
 
 
-    //top, right, bottom, left  
+
+    this.checkNeighbors = function() {
+
+        let neighbors = [];
+
+
+        let top = grid[index(i, j - 1)];
+        let right = grid[index(i + 1, j)];
+        let bottom = grid[index(i, j + 1)];
+        let left = grid[index(i - 1, j)];
+
+
+        if (top && !top.visited) {
+            neighbors.push(top);
+        }
+        if (right && !right.visited) {
+            neighbors.push(right)
+        }
+        if (bottom && !bottom.visited) {
+            neighbors.push(bottom);
+        }
+        if (left && !left.visited) {
+            neighbors.push(left)
+        }
+
+
+        if (neighbors.length > 0) {
+            let r = floor(random(0, neighbors.length));
+            return neighbors[r];
+        } else {
+            return undefined;
+        }
+
+    }
+
 
     this.show = function() {
         let x = this.i * w;
@@ -61,8 +121,14 @@ function Cell(i, j) {
             line(x, y + w, x, y);
         }
 
-        // noFill();
-        // rect(x, y, w, w);
+
+        if (this.visited) {
+
+            fill(255, 0, 255, 100);
+            rect(x, y, w, w);
+
+
+        }
     }
 
 }
